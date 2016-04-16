1f1853d46a82b7a35963a0739463bb9d3bd565ff
Rocky Murdoch <rockysta009@gmail.com>
2016-04-15 17:11:32 -0700

    (refactor/feat) add polling function and refactor to optimise performance
    *scope is only being updated with new data
    *made code DRY

--- client/app/profile/profileCtrl.js
+++ client/app/profile/profileCtrl.js
@@ -2,9 +2,9 @@ angular
   .module('app.profile', ['angularMoment'])
   .controller('ProfileController', ProfileController);
 
-ProfileController.$inject = ['$scope', 'auth', 'Profile'];
+ProfileController.$inject = ['$scope', '$timeout', 'auth', 'Profile'];
 
-function ProfileController($scope, auth, Profile) {
+function ProfileController($scope, $timeout, auth, Profile) {
   // User profile information from Auth0 db
   $scope.profile = auth.profile;
   // User information from our MongoDB
@@ -12,8 +12,11 @@ function ProfileController($scope, auth, Profile) {
   // Form input fields
   $scope.input = {};
 
+  var user_id = $scope.profile.user_id;
+  var currentCount;
+
   $scope.getProfile = function() {
-    Profile.getProfile($scope.profile.user_id)
+    Profile.getProfile(user_id)
       .then(function(data) {
         $scope.user.info = data;
       })
@@ -23,11 +26,12 @@ function ProfileController($scope, auth, Profile) {
   };
 
   $scope.getPosts = function() {
-    Profile.getPosts($scope.profile.user_id)
+    Profile.getPosts(user_id)
       .then(function(data) {
         $scope.user.goals = data.goals;
         $scope.user.posts = data.posts;
         $scope.input.selected = $scope.user.goals[0];
+        currentCount = Profile.countComment(data.posts);
       })
       .catch(function(error) {
         console.error(error);
@@ -35,7 +39,6 @@ function ProfileController($scope, auth, Profile) {
   };
 
   $scope.addPost = function() {
-    var user_id = $scope.profile.user_id;
     var post = {
       post: $scope.input.post,
       goal_id: $scope.input.selected._id,
@@ -51,19 +54,36 @@ function ProfileController($scope, auth, Profile) {
   };
 
   $scope.addComment = function(post_id, goal_id, input) {
-    var user_id = $scope.profile.user_id;
     Profile.addComment(user_id, goal_id, post_id, input)
       .then(function(data) {
-        $scope.getPosts();
+        Profile.pushComment(data, $scope.user.posts, currentCount);
       })
       .catch(function(error) {
         console.error(error);
       });
   };
 
+  $scope.poller = function() {
+    var newPosts = [];
+    Profile.getPosts(user_id)
+      .then(function(data) {
+        newPosts = data.posts;
+        var newCount = Profile.countComment(newPosts);
+        return newCount;
+      })
+      .then(function(newCount) {
+        Profile.checkComment(currentCount, newCount, $scope.user.posts, newPosts);
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+    $timeout($scope.poller, 2000);
+  };
+
   // Once auth0 profile info has been set, query our database for user's profile and posts
   auth.profilePromise.then(function(profile) {
     $scope.getProfile();
     $scope.getPosts();
+    $scope.poller();
   });
 }
