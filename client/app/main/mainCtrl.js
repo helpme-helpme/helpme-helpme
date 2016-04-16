8ffaf458313034c52f4aebac3f11f11052b5f51b
Rocky Murdoch <rockysta009@gmail.com>
2016-04-15 17:09:56 -0700

    (refactor) refactor mainCtrl to use profileFactory methods

--- client/app/main/mainCtrl.js
+++ client/app/main/mainCtrl.js
@@ -36,7 +36,6 @@ function MainController($scope, $timeout, auth, Goals, Friend, Profile) {
     $scope.posts = [];
     Friend.getFriendsPosts(user_id)
       .then(function(data) {
-        console.log(data, "data from backend");
         data.forEach(function(obj) {
           var friend = {};
           friend.firstname = obj[0].firstname || '';
@@ -58,16 +57,7 @@ function MainController($scope, $timeout, auth, Goals, Friend, Profile) {
   $scope.addComment = function(post_id, goal_id, input, friend_id) {
     Profile.addComment(user_id, goal_id, post_id, input, friend_id)
       .then(function(data) {
-        for(var i = 0; i < $scope.posts.length; i++) {
-          var post = $scope.posts[i];
-          var last = data.comments.length-1;
-
-          if (post._id === data._id) {
-            var newComment = data.comments[last];
-            post.comments.push(newComment);
-            return;
-          }
-        }
+        Profile.pushComment(data, $scope.posts, currentCount);
       })
       .catch(function(error) {
         console.error(error);
@@ -76,47 +66,25 @@ function MainController($scope, $timeout, auth, Goals, Friend, Profile) {
 
   $scope.poller = function() {
     // recreate an array similar to $scope.posts
-    var postArray = [];
+    var newPosts = [];
     Friend.getFriendsPosts(user_id)
       .then(function(data) {
-        data.forEach(function(obj) {
-          var friend = {};
-          friend.firstname = obj[0].firstname || '';
-          friend.lastname = obj[0].lastname || '';
-          friend.username = obj[0].username || '';
-          friend.auth_id = obj[0].auth_id;
-          obj[1].forEach(function(post) {
-            post.friend = friend;
-            postArray.push(post);
+        data.forEach(function(friend) {
+          friend[1].forEach(function(post) {
+            newPosts.push(post);
           });
         });
         // count the comment in each post
-        var newCount = Profile.countComment(posts);
-        return postArray;
+        var newCount = Profile.countComment(newPosts);
+        return newCount;
       })
-      .then(function(posts) {
-        for(var post in currentCount) {
-          // check difference in comment count in each post
-          if(currentCount[post] !== newCount[post]) {
-            for(var i = 0; i < $scope.posts.length; i++) {
-              var obj = $scope.posts[i];
-              // find the relevant post in the $scope.posts
-              if(obj.post === post) {
-                // push the new comment in the post
-                var lastIndex = posts[i].comments.length-1;
-                var newComment = posts[i].comments[lastIndex];
-                obj.comments.push(newComment);
-                // make commentCount same to count
-                commentCount = count;
-              }
-            }
-          }
-        }
+      .then(function(newCount) {
+        Profile.checkComment(currentCount, newCount, $scope.posts, newPosts);
       })
       .catch(function(error) {
         console.error(error);
       });
-      $timeout($scope.poller, 5000);
+    $timeout($scope.poller, 2000);
   };
   // Once auth0 profile info has been set, query our database for friends' posts, inactive friends and personal goals.
   auth.profilePromise.then(function(profile) {
